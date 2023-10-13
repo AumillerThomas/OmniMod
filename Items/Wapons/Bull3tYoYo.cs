de93d5a3175600ab6584716ccd23b1c3e26d8cff
AumillerThomas <Aumiller95@web.de>
2023-10-13 13:34:24 +0200

    Bullet Yoyo und Projectile added

---
+++ Items/Wapons/Bull3tYoYo.cs
@@ -0,0 +1,73 @@
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using OmniMod.Items.Projectiles;
+
+namespace OmniMod.Items.Wapons;
+
+public class Bull3tYoYo : ModItem{
+
+     public override void SetStaticDefaults() {
+			// These are all related to gamepad controls and don't seem to affect anything else
+			ItemID.Sets.Yoyo[Item.type] = true; // Used to increase the gamepad range when using Strings.
+			ItemID.Sets.GamepadExtraRange[Item.type] = 50; // Increases the gamepad range. Some vanilla values: 4 (Wood), 10 (Valor), 13 (Yelets), 18 (The Eye of Cthulhu), 21 (Terrarian).
+			ItemID.Sets.GamepadSmartQuickReach[Item.type] = true; // Unused, but weapons that require aiming on the screen are in this set.
+		}
+
+		public override void SetDefaults() {
+			Item.width = 24; // The width of the item's hitbox.
+			Item.height = 24; // The height of the item's hitbox.
+
+			Item.useStyle = ItemUseStyleID.Shoot; // The way the item is used (e.g. swinging, throwing, etc.)
+			Item.useTime = 999; // All vanilla yoyos have a useTime of 25.
+			Item.useAnimation = 999; // All vanilla yoyos have a useAnimation of 25.
+			Item.noMelee = true; // This makes it so the item doesn't do damage to enemies (the projectile does that).
+			Item.noUseGraphic = true; // Makes the item invisible while using it (the projectile is the visible part).
+			Item.UseSound = SoundID.Item1; // The sound that will play when the item is used.
+
+			Item.damage = 64; // The amount of damage the item does to an enemy or player.
+			Item.DamageType = DamageClass.MeleeNoSpeed; // The type of damage the weapon does. MeleeNoSpeed means the item will not scale with attack speed.
+			Item.knockBack = 16.5f; // The amount of knockback the item inflicts.
+			Item.crit = 15; // The percent chance for the weapon to deal a critical strike. Defaults to 4.
+			Item.channel = true; // Set to true for items that require the attack button to be held out (e.g. yoyos and magic missile weapons)
+			Item.rare = ItemRarityID.Orange; // The item's rarity. This changes the color of the item's name.
+			Item.value = Item.buyPrice(copper: 1); // The amount of money that the item is can be bought for.
+
+			Item.shoot = ModContent.ProjectileType<StoepsisYoYoProjectile>(); // Which projectile this item will shoot. We set this to our corresponding projectile.
+			Item.shootSpeed = 26f; // The velocity of the shot projectile.			
+		}
+
+		// Here is an example of blacklisting certain modifiers. Remove this section for standard vanilla behavior.
+		// In this example, we are blacklisting the ones that reduce damage of a melee weapon.
+		// Make sure that your item can even receive these prefixes (check the vanilla wiki on prefixes).
+		private static readonly int[] unwantedPrefixes = new int[] { PrefixID.Terrible, PrefixID.Dull, PrefixID.Shameful, PrefixID.Annoying, PrefixID.Broken, PrefixID.Damaged, PrefixID.Shoddy };
+
+		public override bool AllowPrefix(int pre) {
+			// return false to make the game reroll the prefix.
+
+			// DON'T DO THIS BY ITSELF:
+			// return false;
+			// This will get the game stuck because it will try to reroll every time. Instead, make it have a chance to return true.
+
+			if (Array.IndexOf(unwantedPrefixes, pre) > -1) {
+				// IndexOf returns a positive index of the element you search for. If not found, it's less than 0.
+				// Here we check if the selected prefix is positive (it was found).
+				// If so, we found a prefix that we don't want. Reroll.
+				return false;
+			}
+
+			// Don't reroll
+			return true;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 6);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+
+}
